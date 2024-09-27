57ee595d698b8ad6e0c2b6a3a243830460446b0a
Musaddik09 <121163232+Musaddik09@users.noreply.github.com>
2024-09-27 12:46:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ document.getElementById('quizForm').addEventListener('submit', function (event) 
     localStorage.setItem('student_' + student.rollNumber, JSON.stringify(student));
 
             // Redirect to the 4th website (Admin marks view)
-            window.location.href = "https://a80896b0-6d0b-47d7-82e0-52bc989096a1-00-1eg4giqo1k1k9.pike.replit.dev/"; 
+            window.location.href = "https://musaddik09.github.io/Result/" 
 
 
     // Optionally reset the form after submission
